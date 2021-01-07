9c3e7eab4566a8951bc3c80c339b5fa4f73c5c03
flexiafang <1208689216@qq.com>
2021-01-07 21:53:52 +0800

    go day01 hello

--- src/basic/01_hello/hello.go
+++ src/basic/01_hello/hello.go
@@ -1,16 +1,16 @@
-// 文件所属的包，Go语言中主函数所在的包一定是main
+// 文件所属的包，Go语言中主函数所在的包一定是 main
 package main
 
 // 导入标准输入输出包
 import "fmt"
 
-// func表示函数，Hello是函数名
+// func 表示函数，hello是函数名
 func hello(str string) {
 	fmt.Println(str)
 	println(str)
 }
 
-// 主函数，程序有且只有一个主函数入口
+// main 函数，主函数，程序有且只有一个主函数入口
 func main() {
 	var str string = "hello, world!"
 	hello(str)
